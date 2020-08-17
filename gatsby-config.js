05c320d635b66229ed679d432a0375aff3524513
spences10 <spences10apps@gmail.com>
2020-08-17 18:08:58 +0100

    use MDX, why not!

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
